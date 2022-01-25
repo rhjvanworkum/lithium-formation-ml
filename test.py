08173c5941f774ff98f2b2d638be6d06a26edb07
sdf <rhjvanworkum@gmail.com>
2022-01-25 10:09:39 +0100

    new test run

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
-from pymatgen.ext.matproj import MPRester
+class comp:
+    
+    def __init__(self) -> None:
+        self.element_composition = "hallo"
 
-if __name__ == "__main__":
-    MAPI_KEY = 'vitvGcsFqw8QTTDZ5L'  # You must change this to your Materials API key! (or set MAPI_KEY env variable)
-    # QUERY = "mp-1203"  # change this to the mp-id of your compound of interest
-    QUERY = "TiO"  # change this to a formula of interest
-    # QUERY = "Ti-O"  # change this to a chemical system of interest
+composition = comp()
+print(*composition.element_composition)
+# from matminer.featurizers import composition as cf
 
-    mpr = MPRester(MAPI_KEY)  # object for connecting to MP Rest interface
+# elem = cf.ElementProperty.from_preset("magpie")
 
-    structures = mpr.get_structures(QUERY)
-    for s in structures:
-        print(s)
+# feat = elem.featurize(composition)
+# print(feat)
